c5296637139e82b600e2046409635d869f989e68
Datikzz <einsamejungen@gmail.com>
2022-09-01 15:06:04 +0300

    edit import leaflet styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@ import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	plugins: ['~/plugins/leaflet'],
+	plugins: [{ src: '~/plugins/leaflet', mode: 'client' }],
 });
